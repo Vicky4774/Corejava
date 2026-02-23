160690d8cf470019ee20367b0da604668698c814
Vicky4774 <latakevicky@gmail.com>
2026-02-23 19:22:59 +0530

    Add files via upload

---
+++ Bank.java
@@ -0,0 +1,17 @@
+package com.abc;
+
+public class Bank {
+
+	public static void main(String[] args) 
+	{
+		
+		
+		System.out.println("Bank IFSC Code : "+"IPOS0000002");
+		System.out.println("Bank Name : "+"Bank Of India");
+		System.out.println("Bank Account Holder Name : "+"Vicky Latake");
+		System.out.println("Account Number : "+"65465446654646");
+		System.out.println("Bank Address  : "+"Solapur");
+	}
+
+	
+}
