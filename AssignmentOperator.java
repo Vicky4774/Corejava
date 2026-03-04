122eb1fca29c1fc7c51e16055383e95e8bc63496
Vicky4774 <latakevicky@gmail.com>
2026-03-04 12:19:26 +0530

    Add files via upload

---
+++ AssignmentOperator.java
@@ -0,0 +1,28 @@
+package com.abc;
+
+public class AssignmentOperator {
+
+	public static void main(String[] args) 
+	{
+		int i = 5;
+		--i; 
+		System.out.println(i++); //4
+		++i; 
+		System.out.println(i % 6); //0
+		i++; 
+		System.out.println(i+=2); //9
+		i++; 
+		int b = i +8;
+		System.out.println(i++); //10
+		i++;
+		i /= 4;//3
+		System.out.println(i++); //3
+		--i;
+		System.out.println(i + 4);//7
+
+		System.out.println("______________________________");
+		System.out.println(i);	//3
+
+	}
+
+}
