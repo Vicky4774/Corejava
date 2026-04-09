5e39d21ab842eb49a58c76d7701cc768777e162f
Vicky4774 <latakevicky@gmail.com>
2026-04-09 22:00:25 +0530

    Add files via upload

---
+++ abstra/SavingAccount.java
@@ -0,0 +1,13 @@
+package com.abstra;
+
+import com.xyz.Bank;
+
+public class SavingAccount extends Bank {
+
+	@Override
+	public void calculateInterest() {
+		System.out.println("The Saving Account Interest Is 9 %");
+		
+	}
+
+}
