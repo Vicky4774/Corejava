1c77c2ed05e6c50463699f330afad7a59157512d
Vicky4774 <latakevicky@gmail.com>
2026-04-09 21:54:38 +0530

    Add files via upload

---
+++ abstra/CurrentAccount.java
@@ -0,0 +1,18 @@
+package com.abstra;
+
+import com.xyz.Bank;
+
+public class CurrentAccount extends Bank
+{
+
+	@Override
+	public void calculateInterest() {
+		
+		System.out.println("The Current Account Interest Is 5 % ");
+		
+	}
+	
+	
+	
+
+}
