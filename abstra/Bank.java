fc15e9de6ce4f15bdd08eddd30e42f07e5e462ea
Vicky4774 <latakevicky@gmail.com>
2026-04-09 21:57:10 +0530

    Add files via upload

---
+++ abstra/Bank.java
@@ -0,0 +1,16 @@
+package com.abstra;
+
+public abstract class Bank 
+
+{
+	public abstract void calculateInterest();	//abstract method does not have any body of the method.
+	
+	public void displayBalance()
+	{
+		System.out.println("Display Balance Method..!");
+	}
+	
+
+
+	
+}
