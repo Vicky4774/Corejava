d7cbc159cb983198b03763a541626c4089586802
Vicky4774 <latakevicky@gmail.com>
2026-04-09 21:58:37 +0530

    Add files via upload

---
+++ abstra/Employee.java
@@ -0,0 +1,13 @@
+package com.abstra;
+
+public abstract class Employee {
+	
+	
+	public abstract void calculatesalary();
+	public void details()
+	{
+		System.out.println("The Employee Name" +"Ram");
+	}
+	
+
+}
