887761de5e5ff9ffbb848dc2a38a085033f841ed
Vicky4774 <latakevicky@gmail.com>
2026-04-11 22:19:00 +0530

    Add files via upload

---
+++ ProgrammingSeries/bin/com/logic/Demo.java
@@ -0,0 +1,23 @@
+package com.logic;
+
+public class Demo {
+
+	public static void main(String[] args) 
+	{
+		
+		for(int i=0;i<=100;i++)
+		{
+			if(i%2==0)
+			{
+				if(i>=50)
+				{
+					
+					System.out.println("Even Number : "+i);
+				}
+			}
+		}
+		
+		
+	}
+
+}
