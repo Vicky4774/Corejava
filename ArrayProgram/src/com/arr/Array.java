29ee64a628fbb61a1c9fa80da8a69133a10be890
Vicky4774 <latakevicky@gmail.com>
2026-04-14 15:48:57 +0530

    Add files via upload

---
+++ ArrayProgram/src/com/arr/Array.java
@@ -0,0 +1,17 @@
+package com.arr;
+
+public class Array {
+
+	public static void main(String[] args)
+	{
+		int arr[]= {10,20,30,40,50,60};
+		{
+			for(int i=0;i<arr.length;i++)
+			{
+				System.out.println("Elements of the array : "+arr[i]);
+				
+			}
+		}
+	}
+
+}
