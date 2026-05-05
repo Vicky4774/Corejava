a451962b5465f2bd6ac43b558192d2c3e51b2a50
Vicky4774 <latakevicky@gmail.com>
2026-05-05 11:33:23 +0530

    Add files via upload

---
+++ Collection/bin/com/collection/RemoveDuplicate.java
@@ -0,0 +1,24 @@
+package com.collection;
+
+import java.util.*;
+
+public class RemoveDuplicate 
+{
+    public static void main(String[] args) 
+    {
+        ArrayList<Integer> list = new ArrayList<>();
+
+        list.add(1);
+        list.add(2);
+        list.add(2);
+        list.add(3);
+        list.add(4);
+        list.add(5);
+        list.add(6);
+        
+        
+        HashSet<Integer> set = new HashSet<>(list);
+
+        System.out.println(set);
+    }
+}
