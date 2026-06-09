6c382c9955cca5e013c1ad6ef155a6326dcfe3db
Vicky4774 <latakevicky@gmail.com>
2026-06-09 21:07:50 +0530

    Add files via upload

---
+++ LogicBuilding/src/com/jbk/SwappingNumber.java
@@ -0,0 +1,21 @@
+package com.jbk;
+
+public class SwappingNumber {
+
+	public static void main(String[] args) 
+	{
+		int a=10;
+		int b=20;
+		
+		System.out.println("Before Swapping " +a +" "  +b );
+		int temp=a;  //temp =10 
+		a=b;		//b=10
+		b=temp;
+		System.out.println("After Swapping " +a + " " +  +b );
+
+		
+		
+
+	}
+
+}
